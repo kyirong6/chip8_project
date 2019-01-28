c8bbe210b5bac2980cf3ee8a834edbf128553dfc
zyjiang1998 <46609437+zyjiang1998@users.noreply.github.com>
2019-01-28 13:25:37 -0800

    moved from chip8.js to CPU.js

--- src/chip8.js
+++ src/chip8.js
@@ -25,48 +25,6 @@ class chip8 {
     this._cpu = new CPU(this._memory, this._input, this._display);
   }
 
-  // take opcode and perform operation
-
-  emulateCycle: function(){
-    var opcode = this._memory[this.pc];
-    this.pc += 2;
-    switch (opcode & 0xf000){
-      var x = (opcode & 0x0F00) >> 8;
-      var y = (opcode & 0x00F0) >> 4;
-
-      case 0x5000:
-        if(this._v[x] == this._v[y]){
-          this.pc += 2;
-        }
-        break;
-      case 0x6000:
-        this._v[x] = opcode & 0xFF
-        break;
-
-      case 0x7000:
-        var val = (opcode & 0xFF + this._v[x]);
-        if (val > 255){ val -= 256;}
-        this._v[x] = val;
-        break;
-
-      case 0x8000:
-        switch(opcode & 0x000f){
-          case 0x0000:
-            this._v[x] = this._v[y];
-            break;
-
-          case 0x0001:
-            this._v[x] |= this._v[y];
-            break;
-
-          case 0x0002:
-            this._v[x] &= this._v[y];
-
-          case 0x0003:
-            thisv.[x] ^= this._v[y];
-        }
-    }
-
 
     /*
     A method to load the chip8 program
--- src/components/CPU.js
+++ src/components/CPU.js
@@ -46,51 +46,84 @@ class CPU {
     var y = (opcode & 0x00F0) >> 4; // isolate variable y from opcode
 
 	switch(opcode & 0xF000){ //grab the first nibble
-	
+
 		case 0x0000:
 			switch(opcode){
 				//  0NNN (ignored by modern interpreters)
 				case 0x00E0: //	00E0 Clears Screen
 				//display.clear() to be added later
 				break;
-			
+
 				case 0x00EE: //	00EE Returns from Subroutine
 				this._pc = this.stack[this._sp - 1];
 				break;
 			}
 		break;
-		
+
 		//	1NNN Jumps to address NNN *Done*
 		case 0x1000:
-			this._pc = opcode & 0x0FFF; 
+			this._pc = opcode & 0x0FFF;
 		break;
-		
+
 		//  2NNN Calls subroutine at NNN
 		case 0x2000:
 			this._stack[this._sp] = this._pc; 		//saves pc in the stacks
 			this._sp++;						  		//add one to the stackpointer to avoid collision
 			this._pc = (opcode & 0x0FFF);		    //grab last 3 nibbles of the opcode
 		break;
-		
+
 		//  3XNN Skips the next instruction if VX equals NN;
-		case 0x3000: 
+		case 0x3000:
 			if(this._v[x] == (opcode & 0x00FF)){	//check if last 2 nibbles are equal to VX
 				this._pc += 2
 			}
 		break;
-		
+
 		//	4XNN Skips the next instruction if VX doesn't equal NN
-		case 0x4000: 
+		case 0x4000:
 		if(this._v[x] != (opcode & 0x00FF)){	//check if last 2 nibbles are NOT equal to VX
 			this._pc += 2
 		}
 		break;
-		
+
+    case 0x5000:
+      if(this._v[x] == this._v[y]){
+        this.pc += 2;
+      }
+      break;
+      
+    case 0x6000:
+      this._v[x] = opcode & 0xFF
+      break;
+
+    case 0x7000:
+      var val = (opcode & 0xFF + this._v[x]);
+      if (val > 255){ val -= 256;}
+      this._v[x] = val;
+      break;
+
+    case 0x8000:
+      switch(opcode & 0x000f){
+        case 0x0000:
+          this._v[x] = this._v[y];
+          break;
+
+        case 0x0001:
+          this._v[x] |= this._v[y];
+          break;
+
+        case 0x0002:
+          this._v[x] &= this._v[y];
+
+        case 0x0003:
+          thisv.[x] ^= this._v[y];
+      }
+
 	}
-	
+
     // TODO: add switch statements to process the opcode needed
-		
-		
-  
+
+
+
 
 }
