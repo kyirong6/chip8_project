9cd5f0ac61f3fc6c556a9b719e2e0d387de482bf
Choenden Kyirong <choendenkyirong@d142-058-221-024.wireless.sfu.ca>
2019-01-28 12:59:22 -0800

    added memory functionality

--- src/components/Memory.js
+++ src/components/Memory.js
@@ -1,6 +1,21 @@
 class Memory {
-	
+
+	constructor() {
+		this._mem = new ArrayBuffer(0xFFF);
+	}
+
+
+	write(location, value) {
+		for (let i = 0; i < value.length; i++) {
+			this._mem[location + i] = value[i];
+		}
+	}
+
+	read(location) {
+		return this._mem[location];
+	}
 }
+
 
 //4096 (0x1000)memory locations
 //chip8 emulator will occupy first 512 bytes
