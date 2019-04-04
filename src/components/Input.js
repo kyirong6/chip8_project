faae4b8691e0c8b6cfcf0c75f3c292b2a3ac49e2
Andrew T <andrewt25@outlook.com>
2019-04-03 20:19:44 -0700

    Fixed Restart

--- src/components/Input.js
+++ src/components/Input.js
@@ -2,9 +2,8 @@ class Input {
 
     constructor(){
 
-        this._keyBoardBuffer = new Uint16Array(16) // keyboard buffer (new Uint16Array(16);)
         this._isKeyPressed = false; // gets set whenever a key is pressed (boolean)
-        this._keyPressed = 0;
+        this._keyPressed = 0xFF;
         document.addEventListener('keyup', (event)=>
         {
             this._isKeyPressed = false;
@@ -80,7 +79,7 @@ class Input {
                     this._isKeyPressed = true;
                     break;
                 default:
-                    this._keyPressed = 0;
+                    this._keyPressed = 0xFF;
             }
         });
     }
@@ -96,7 +95,7 @@ class Input {
   }
   clear()
   {
-      this._keyPressed = 0;
+      this._keyPressed = 0xFF;
   }
   getCode()
   {
